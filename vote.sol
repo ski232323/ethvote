8163abb7be6f23386f0306ff67a767672c3d2f47
ski232323 <152718343+ski232323@users.noreply.github.com>
2025-08-20 19:50:42 +0200

    Update vote.sol

--- vote.sol
+++ vote.sol
@@ -1,86 +1,298 @@
-// SPDX-License-Identifier: MIT
+
+/**
+ *Submitted for verification at Etherscan.io on 2025-07-25
+*/
+
+// File: @openzeppelin/contracts/utils/Context.sol
+
+
+// OpenZeppelin Contracts (last updated v5.0.1) (utils/Context.sol)
+
 pragma solidity ^0.8.20;
 
-interface IVotes {
-    function getVotes(address account) external view returns (uint256);
-    function getPastTotalSupply(uint256 blockNumber) external view returns (uint256);
+/**
+ * @dev Provides information about the current execution context, including the
+ * sender of the transaction and its data. While these are generally available
+ * via msg.sender and msg.data, they should not be accessed in such a direct
+ * manner, since when dealing with meta-transactions the account sending and
+ * paying for execution may not be the actual sender (as far as an application
+ * is concerned).
+ *
+ * This contract is only required for intermediate, library-like contracts.
+ */
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        return msg.data;
+    }
+
+    function _contextSuffixLength() internal view virtual returns (uint256) {
+        return 0;
+    }
 }
 
-contract SimpleVoting {
-    IVotes public token;
-    address public owner;
-    bool public votingEnded;
+// File: @openzeppelin/contracts/access/Ownable.sol
 
+
+// OpenZeppelin Contracts (last updated v5.0.0) (access/Ownable.sol)
+
+pragma solidity ^0.8.20;
+
+
+/**
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * The initial owner is set to the address provided by the deployer. This can
+ * later be changed with {transferOwnership}.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * the owner.
+ */
+abstract contract Ownable is Context {
+    address private _owner;
+
+    /**
+     * @dev The caller account is not authorized to perform an operation.
+     */
+    error OwnableUnauthorizedAccount(address account);
+
+    /**
+     * @dev The owner is not a valid owner account. (eg. `address(0)`)
+     */
+    error OwnableInvalidOwner(address owner);
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the address provided by the deployer as the initial owner.
+     */
+    constructor(address initialOwner) {
+        if (initialOwner == address(0)) {
+            revert OwnableInvalidOwner(address(0));
+        }
+        _transferOwnership(initialOwner);
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        _checkOwner();
+        _;
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if the sender is not the owner.
+     */
+    function _checkOwner() internal view virtual {
+        if (owner() != _msgSender()) {
+            revert OwnableUnauthorizedAccount(_msgSender());
+        }
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby disabling any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        _transferOwnership(address(0));
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        if (newOwner == address(0)) {
+            revert OwnableInvalidOwner(address(0));
+        }
+        _transferOwnership(newOwner);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Internal function without access restriction.
+     */
+    function _transferOwnership(address newOwner) internal virtual {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+}
+
+// File: @openzeppelin/contracts/token/ERC20/IERC20.sol
+
+
+// OpenZeppelin Contracts (last updated v5.4.0) (token/ERC20/IERC20.sol)
+
+pragma solidity >=0.4.16;
+
+/**
+ * @dev Interface of the ERC-20 standard as defined in the ERC.
+ */
+interface IERC20 {
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    /**
+     * @dev Returns the value of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the value of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves a `value` amount of tokens from the caller's account to `to`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address to, uint256 value) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    /**
+     * @dev Sets a `value` amount of tokens as the allowance of `spender` over the
+     * caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 value) external returns (bool);
+
+    /**
+     * @dev Moves a `value` amount of tokens from `from` to `to` using the
+     * allowance mechanism. `value` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
+}
+
+// File: vote.sol
+
+
+pragma solidity ^0.8.27;
+
+
+
+contract TokenBasedVoting is Ownable {
     struct Proposal {
         string description;
-        uint256 voteCount;
+        uint voteCount;
     }
 
     Proposal[] public proposals;
+    mapping(uint => mapping(address => bool)) public hasVoted;
 
-    mapping(address => bool) public hasVoted;
+    bool public votingActive;
+    uint public winningProposalId;
+    uint public currentVoteId;
+    IERC20 public votingToken;
 
-    uint256 public votingBlock; // bloc où on prend le snapshot de la supply
+    constructor(address _tokenAddress) Ownable(msg.sender) { // <-- ici on passe msg.sender
+        votingToken = IERC20(_tokenAddress);
+    }
 
-    constructor(address tokenAddress, string[] memory proposalDescriptions) {
-        owner = msg.sender;
-        token = IVotes(tokenAddress);
+    function startNewVote(string[] calldata proposalDescriptions) external onlyOwner {
+        require(!votingActive, "Voting already active");
+
+        delete proposals;
         for (uint i = 0; i < proposalDescriptions.length; i++) {
             proposals.push(Proposal({
                 description: proposalDescriptions[i],
                 voteCount: 0
             }));
         }
-        votingBlock = block.number;
-        votingEnded = false;
+
+        votingActive = true;
+        winningProposalId = 0;
+        currentVoteId += 1;
     }
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Not owner");
-        _;
+    function vote(uint proposalId) external {
+        require(votingActive, "Voting is not active");
+        require(!hasVoted[currentVoteId][msg.sender], "Already voted in this vote");
+        require(proposalId < proposals.length, "Invalid proposal");
+
+        uint voterBalance = votingToken.balanceOf(msg.sender);
+        require(voterBalance > 0, "Need voting tokens to vote");
+
+        proposals[proposalId].voteCount += voterBalance;
+        hasVoted[currentVoteId][msg.sender] = true;
     }
 
-    modifier voteAllowed() {
-        require(!votingEnded, "Voting has ended");
-        _;
-    }
+    function endVoting() external onlyOwner {
+        require(votingActive, "Voting already ended");
 
-    function vote(uint proposalId) public voteAllowed {
-        require(!hasVoted[msg.sender], "Already voted");
-        uint256 voterPower = token.getVotes(msg.sender);
-        require(voterPower > 0, "No voting power");
-        proposals[proposalId].voteCount += voterPower;
-        hasVoted[msg.sender] = true;
-    }
+        votingActive = false;
+        uint maxVotes = 0;
 
-    /// @notice Terminer le vote (seulement propriétaire)
-    function endVoting() public onlyOwner {
-        votingEnded = true;
-    }
-
-    /// @notice Renvoie l'index de la proposition gagnante (celle avec le plus de votes)
-    function winningProposal() public view returns (uint winningProposalId) {
-        uint highestVoteCount = 0;
         for (uint i = 0; i < proposals.length; i++) {
-            if (proposals[i].voteCount > highestVoteCount) {
-                highestVoteCount = proposals[i].voteCount;
+            if (proposals[i].voteCount > maxVotes) {
+                maxVotes = proposals[i].voteCount;
                 winningProposalId = i;
             }
         }
     }
 
-    /// @notice Vérifie si une proposition a la majorité absolue (>50%)
-    function isMajority(uint proposalId) public view returns (bool) {
-        uint totalSupplyAtVote = token.getPastTotalSupply(votingBlock);
-        return proposals[proposalId].voteCount > totalSupplyAtVote / 2;
+    function getWinningProposal() external view returns (string memory description, uint voteCount) {
+        require(!votingActive, "Voting still active");
+        Proposal storage winner = proposals[winningProposalId];
+        return (winner.description, winner.voteCount);
     }
 
-    function getProposal(uint proposalId) public view returns (string memory description, uint256 voteCount) {
-        Proposal storage proposal = proposals[proposalId];
-        return (proposal.description, proposal.voteCount);
-    }
-
-    function getProposalsCount() public view returns (uint) {
+    function getProposalCount() external view returns (uint) {
         return proposals.length;
     }
 }
+
